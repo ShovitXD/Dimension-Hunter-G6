3843e1f9fccf21d75d2e124b6a3f98f06091a7f9
Shovit <shovitnaik117@gmail.com>
2025-04-23 23:42:32 +0100

    UPDATE
    Added UI update and Team Chnage update

--- Assets/0Game/Scripts/Core/GameManager.cs
+++ Assets/0Game/Scripts/Core/GameManager.cs
@@ -4,6 +4,8 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
     public string RoomCode { get; private set; }
+
+    public static int playerTeam = -1;
     private void Awake()
     {
         if (Instance == null)
@@ -21,5 +23,15 @@ public class GameManager : MonoBehaviour
     public void GenerateRoomCode()
     {
         RoomCode = Random.Range(1000, 9999).ToString();
+    }
+
+    public void AssignTeam(int team)
+    {
+        playerTeam = team;
+
+    }
+    public int GetPlayerTeam()
+    {
+        return playerTeam;
     }
 }
